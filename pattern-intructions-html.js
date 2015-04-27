b0cc73659bc3c46458645118f3b3c2614dab0fda
David Brokaw <dgbrokaw@indiana.edu>
2015-04-27 10:57:38 -0400

    white background

---
+++ pattern-intructions-html.js
@@ -0,0 +1,45 @@
+patternInstructionPage =
+
+"<div id='instructionDiv'>" +
+
+	"<div id='instructionText'>" +
+
+		"The possible pairwise patterns are available:" +
+
+	"</div>" +
+
+	"<div id='instructionText'>" +
+
+		"<strong><ins>Shape-based patterns:</strong></ins>" +
+
+		"<img src='patternImages/Same-Shape.png' height='20%' width='30%'>" +
+
+		"<img src='patternImages/Circle-Square-Triangle.png' height='20%' width='30%'>" +
+
+		"<img src='patternImages/Circle-Triangle-Square.png' height='20%' width='30%'>" +
+
+	"</div>" +
+
+	"<div id='instructionText'>" +
+
+		"<strong><ins>Size-based patterns:</strong></ins>" +
+
+		"<img src='patternImages/Same-Size.png' height='20%' width='30%'>" +
+
+		"<img src='patternImages/Large-Medium-Small.png' height='20%' width='30%'>" +
+
+		"<img src='patternImages/Small-Medium-Large.png' height='20%' width='30%'>" +
+
+	"</div>" +
+
+	"<div id='instructionText'>" +
+
+		"<strong><ins>Fill-based patterns:</strong></ins>" +
+
+		"<img src='patternImages/Same-Fill.png' height='20%' width='30%'>" +
+
+		"<img src='patternImages/Not-Fill-Filled.png' height='20%' width='30%'>" +
+
+	"</div>" +
+
+"</div>";
