6aa861901b6409abde15976c5bb19a0ebc479fd7
David Brokaw <dgbrokaw@indiana.edu>
2015-04-27 14:10:07 -0400

    some new pictures; corrected typos; updated text; only have 5 stimuli

--- instruction-html.js
+++ instruction-html.js
@@ -1,15 +1,18 @@
 instructionPages = [
 
 	[
-		{text: 'For each test trial, there is a 50% chance that right screen was generated completely randomly.  There is also a 50% chance that it was generated according to a pattern that consistently links the objects in the right area with their corresponding paired objects in the left area. When there most likely <i>is not</i> a pattern, you should select "RANDOM" from the drop down menu.'
+		{text: '<p>This is a task about determining whether objects in two scenes may or may not be connected to each other by a <strong>consistent pattern</strong>.</p><p></br>You will be shown two screens in which pairs of objects will be revealed in a random sequence. Your job is to determine whether the paired objects revealed in the right and left screens follow a consistent pattern.</p><p></br>The objects in each pair wil always occur with the <strong><em>same location and same color</em></strong> in both screens. Objects occurring in different locations or with different colors in the two screens do not belong to the same pair.</p>'
+		,image: 'Page-2'}
+
+	 ,{text: 'For each test trial, there is a 50% chance that right screen was generated completely randomly.  There is also a 50% chance that it was generated according to a pattern that consistently links the objects in the right area with their corresponding paired objects in the left area. When there most likely <i>is not</i> a pattern, you should select "RANDOM" from the drop down menu.'
 	  ,image: 'Page-3'}
 
-	 ,{text: '<p>When there most likely <i>is</i> a pattern you should select the pattern that you believe is most probable. There are eight (8) possible patterns to choose from. No other patterns are used. The eight patterns are each based on a single feature.</p><p>For a pattern to be true, it must be true for all pairs in the two screens. Combinations of patterns are not used.</p>'
+	 ,{text: '<p>When there most likely <i>is</i> a pattern you should select the pattern that you believe is most probable. There are eight (8) possible patterns to choose from. No other patterns are used. The eight patterns are each based on a single feature.</p><p>For a pattern to be true, it must be true for <strong>all pairs</strong> in the two screens. Combinations of patterns are not used.</p>'
 	  ,image: 'Page-4'}
 	]
  ,[
 
-	  {text: '<p>You will reveal objects by clicking the "Show me more" button. The boxes above the dark areas will highlight to show there are new objects.</p><p></br>Each time a full pair is revealed, and you are required to select the most likely pattern from the dropdown menu and to indicate your level of confidence in the selected pattern. You will continue revealing objects and updating your pattern and confidence selections until all shapes have been revealed.</p><p></br>At any time you may view the eight patterns by pressing the "VIEW PATTERNS" button. The patterns will be shown in a pop up window. You must close this window to return to the trials. An image describing your presently selected pattern will appear at the bottom left side of the screen.</p>'
+	  {text: '<p>You will reveal objects by clicking the "Show me more" button on the bottom right of the screen which will reveal a number of objects in both screens, leading to one or more new complete pairs.</p><p></br>Each time that a full pair is newly revealed you are required to select the most likely pattern from the dropdown menu and to indicate your level of confidence in the selected pattern by adjusting the slider. During this time you may not press the "Show me more" button and it will instead display "Tell us what you think."</p><p></br>You will continue revealing objects and updating your pattern and confidence selections until all shapes have been revealed.</p>'
 	  ,image: 'Page-6'}
 
 	 ,{text: '<p>When all of the objects have been revealed you will have one final chance to update your selections. Then press the "Go to the next area" button to begin the next trial.</p><p></br>There are 10 total trials to complete, which is expected to take 30 minutes. Although, you may take up to 6 minutes per trial for a maximum of 1 hr.</p><p></br>Your pattern selections and confidence levels will be evaluated for probabilistic accuracy and appropriateness accounting for the information presented to you at each turn.</p>'
